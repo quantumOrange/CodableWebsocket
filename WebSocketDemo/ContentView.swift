f9fc76d1cfc70a1056bc94666889cd033c2b5e47
David Crooks <crodave@gmail.com>
2020-09-07 09:57:43 +0200

    Initial Commit

---
+++ WebSocketDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebSocketDemo
+//
+//  Created by David Crooks on 07/09/2020.
+//  Copyright © 2020 David Crooks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
